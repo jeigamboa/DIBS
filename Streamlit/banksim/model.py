ffe35618c1926e19f73976fb4c9339928e67ff47
mitchiroux <brkong@up.edu.ph>
2025-08-08 16:20:17 +0800

    added time series data from a simulated day

--- Streamlit/banksim/model.py
+++ Streamlit/banksim/model.py
@@ -8,6 +8,39 @@ N_OPERATORS = 2
 N_LONG_OPERATORS = 2 #Long transactions such as loan applications and financial consultation
 
 
+#Queueing behavior parameters
+#for get_dynamic_mean_iat(current_time, PEAK_MIN, PEAK_MIN_STANDARD_DEVIATION, LONG_IAT, SHORT_IAT):
+PEAK_MIN= 4*60 #minute at which peak time is, measured from opening hour (e.g. 180 mins from opening hours)
+PEAK_MIN_STANDARD_DEVIATION= 3*60 #smaller PEAK_MIN_STANDARD_DEVIATION = much much higher traffic during peak hr
+
+##Inter-arrival time
+BASE_MEAN_IAT = 4.0 # default mean inter-arrival time (exp)
+BASE_VAR_IAT =  0.5 #difference between mean inter-arrival time and highest inter-arrival time or lowest inter-arrival time
+LONG_IAT =      BASE_MEAN_IAT+2*BASE_VAR_IAT
+SHORT_IAT=      BASE_MEAN_IAT-BASE_VAR_IAT
+#percent of long transactions that consist of all transactions processed by the branch
+
+LONG_TRANSACT_BASE_MEAN_IAT = 75.0    #CONTROLLABLE
+LONG_TRANSACT_BASE_VAR_IAT = 35.0     #CONTROLLABLE
+LONG_TRANSACT_LONG_IAT =    LONG_TRANSACT_BASE_MEAN_IAT + 2*LONG_TRANSACT_BASE_VAR_IAT        
+LONG_TRANSACT_SHORT_IAT =   LONG_TRANSACT_BASE_MEAN_IAT - LONG_TRANSACT_BASE_VAR_IAT
+
+#simulate queueing behavior inside branch
+#Suppose branches have a particular capacity for a particular queue
+
+AREA_BRANCH =               150.0 #in sq. m 
+AREA_WAIT_RATIO =           0.05 #this should be a 0-> 1 slider in streamlit
+AREA_WAIT =                 AREA_WAIT_RATIO*AREA_BRANCH
+CUSTOMER_PREFERRED_AREA =   1 #suppose a customer likes having 1 sq. m of space while waiting
+#in units of sq. m per person
+#Make this adjustable also. The default should be estimated
+
+CUSTOMER_CAPACITY = int(np.round(AREA_WAIT/CUSTOMER_PREFERRED_AREA)) #calculated customer capacity based on available waiting area and 
+#preferred personal space of customer
+
+
+
+
 # default service time parameters (triangular)
 #This is for short transactions
 CALL_LOW = 5.0
@@ -18,39 +51,6 @@ CALL_HIGH = 10.0
 LONG_CALL_LOW = 60
 LONG_CALL_MODE = 70
 LONG_CALL_HIGH = 100
-
-#for modelling probabilistic behavior
-LOW_UNIF = 0
-HIGH_UNIF = 1
-
-#Queueing behavior parameters
-#for get_dynamic_mean_iat(current_time, PEAK_MIN, PEAK_MIN_STANDARD_DEVIATION, LONG_IAT, SHORT_IAT):
-PEAK_MIN= 4*60 #minute at which peak time is, measured from opening hour (e.g. 180 mins from opening hours)
-PEAK_MIN_STANDARD_DEVIATION= 3*60 #smaller PEAK_MIN_STANDARD_DEVIATION = much much higher traffic during peak hr
-MEAN_IAT = 4 # default mean inter-arrival time (exp)
-LONG_IAT = 5
-SHORT_IAT= 4
-#percent of long transactions that consist of all transactions processed by the branch
-
-LONG_TRANSACT_MEAN_IAT = 75
-LONG_TRANSACT_LONG_IAT = 180
-LONG_TRANSACT_SHORT_IAT = 45
-
-#simulate queueing behavior inside branch
-#Suppose branches have a particular capacity for a particular queue
-
-AREA_BRANCH = 150 #in sq. m  #should be 
-AREA_DESK_OFFICE_RATIO = 0.95 #Suppose 95% of the area of the branch is for desks, offices, etc
-AREA_DESK_OFFICE = AREA_DESK_OFFICE_RATIO*AREA_BRANCH
-AREA_WAIT_RATIO = 1-AREA_DESK_OFFICE_RATIO #this should be a 0-> 1 slider in streamlit
-AREA_WAIT = AREA_WAIT_RATIO*AREA_BRANCH
-CUSTOMER_PREFERRED_AREA = 1 #suppose a customer likes having 1 sq. m of space while waiting
-#in units of sq. m per person
-#Make this adjustable also. The default should be estimated
-
-
-CUSTOMER_CAPACITY = int(np.round(AREA_WAIT/CUSTOMER_PREFERRED_AREA)) #calculated customer capacity based on available waiting area and 
-#preferred personal space of customer
 
 # sampling settings
 N_STREAMS = 4 #DO NOT change
@@ -71,6 +71,16 @@ class Triangular:
         self.high = high
     
     def sample(self, size=None):
+        """
+        Draws a sample from an exponential distribution.
+        
+        Parameters:
+        - size: int or tuple, optional — output shape
+        - mean: float, optional — overrides default_mean for this sample
+
+        Returns:
+        - float or ndarray
+        """
         return self.rand.triangular(self.low, self.mode, self.high, size=size)
 
 class Exponential:
@@ -99,14 +109,23 @@ class NormalDistribution:
         self.std_dev = std_dev
 
     def sample(self, size=None):
+        """
+        Draws a sample from an exponential distribution.
+        
+        Parameters:
+        - size: int or tuple, optional — output shape
+        - mean: float, optional — overrides default_mean for this sample
+
+        Returns:
+        - float or ndarray
+        """
         return self.rand.normal(loc=self.mean, scale=self.std_dev, size=size)
     
 
     
 class Experiment:
     """
-    Encapsulates the concept of an experiment 🧪 with the urgent care
-    call centre simulation model.
+    Encapsulates the concept of an experiment.
 
     An Experiment:
     1. Contains a list of parameters that can be left as defaults or varied
@@ -117,27 +136,29 @@ class Experiment:
 
     def __init__(
         self,
-        random_number_set=DEFAULT_RND_SET,
-        n_operators=N_OPERATORS,
-        n_long_operators=N_LONG_OPERATORS,
-        mean_iat=MEAN_IAT,
-        long_call_low = LONG_CALL_LOW,
-        long_call_mode = LONG_CALL_MODE,
-        long_call_high=LONG_CALL_HIGH,
-        call_low=CALL_LOW,
-        call_mode=CALL_MODE,
-        call_high=CALL_HIGH,
-        short_iat = SHORT_IAT,
-        long_iat = LONG_IAT,
-        peak_min = PEAK_MIN,
-        long_transact_mean_iat = LONG_TRANSACT_MEAN_IAT,
-        long_transact_short_iat =  LONG_TRANSACT_SHORT_IAT,
-        long_transact_long_iat = LONG_TRANSACT_LONG_IAT,
-        peak_min_standard_deviation = PEAK_MIN_STANDARD_DEVIATION,
-        n_streams=N_STREAMS,
-        customer_capacity = CUSTOMER_CAPACITY,
-        low_unif = LOW_UNIF,
-        high_unif=HIGH_UNIF
+        random_number_set=  DEFAULT_RND_SET,
+        n_operators=        N_OPERATORS,
+        n_long_operators=   N_LONG_OPERATORS,
+        base_mean_iat =     BASE_MEAN_IAT,
+        base_var_iat  =     BASE_VAR_IAT,
+        short_iat =         None, #BASE_MEAN_IAT - BASE_VAR_IAT
+        long_iat =          None, #BASE_MEAN_IAT + 2*BASE_VAR_IAT,
+        
+        long_call_low =     LONG_CALL_LOW,
+        long_call_mode =    LONG_CALL_MODE,
+        long_call_high=     LONG_CALL_HIGH,
+        call_low=           CALL_LOW,
+        call_mode=          CALL_MODE,
+        call_high=          CALL_HIGH,
+        
+        peak_min =          PEAK_MIN,
+        long_transact_base_mean_iat =   LONG_TRANSACT_BASE_MEAN_IAT,
+        long_transact_base_var_iat =    LONG_TRANSACT_BASE_VAR_IAT,
+        long_transact_short_iat =       None, #LONG_TRANSACT_BASE_MEAN_IAT - LONG_TRANSACT_BASE_VAR_IAT, 
+        long_transact_long_iat =        None, #LONG_TRANSACT_BASE_MEAN_IAT + 2*LONG_TRANSACT_BASE_VAR_IAT,
+        peak_min_standard_deviation =   PEAK_MIN_STANDARD_DEVIATION,
+        n_streams=                      N_STREAMS,
+        customer_capacity =             CUSTOMER_CAPACITY, #include mechanism to just calculate customer capacity in the website
     ):
         #get_dynamic_mean_iat(current_time, PEAK_MIN, PEAK_MIN_STANDARD_DEVIATION, LONG_IAT, SHORT_IAT):
 
@@ -145,29 +166,35 @@ class Experiment:
         self.random_number_set = random_number_set
         self.n_streams = n_streams
         
-        # store parameters for the run of the model
+        #Resources
         self.n_operators = n_operators
-        self.mean_iat = mean_iat
         self.n_long_operators = n_long_operators
-
-        self.call_low = call_low
-        self.call_mode = call_mode
-        self.call_high = call_high
-        self.long_call_low =long_call_low
-        self.long_call_mode = long_call_mode
-        self.long_call_high = long_call_high
-
-        self.long_transact_long_iat = long_transact_long_iat
-        self.long_transact_short_iat = long_transact_short_iat
-        self.long_transact_mean_iat = long_transact_mean_iat
-
-        self.low_unif = low_unif
-        self.high_unif = high_unif
-
         self.customer_capacity = customer_capacity
-        self.short_iat = short_iat
-        self.long_iat = long_iat
 
+        #parameters for time-varying mean inter-arrival time
+        #Time varying mean inter-arrival time for short transactions
+        self.base_mean_iat = base_mean_iat
+        self.base_var_iat = base_var_iat
+        #use assigned value Experiment(long_iat=value) if assigned. if none assigned, calculate from base_var_iat
+        self.short_iat = short_iat if short_iat is not None else base_mean_iat - base_var_iat
+        self.long_iat = long_iat if short_iat is not None else base_mean_iat + base_var_iat
+
+        #Time varying mean inter-arrival time for long transactions
+        self.long_transact_base_mean_iat = long_transact_base_mean_iat
+        self.long_transact_base_var_iat = long_transact_base_var_iat
+        #use assigned value Experiment(long_transact_long_iat=value) if assigned. if none assigned, calculate from long_transact_base_var_iat
+        self.long_transact_long_iat = long_transact_long_iat if long_transact_long_iat is not None else long_transact_base_mean_iat + long_transact_base_var_iat
+        self.long_transact_short_iat = long_transact_short_iat if long_transact_short_iat is not None else long_transact_base_mean_iat - long_transact_base_var_iat
+
+        #Transaction time parameters
+        self.call_low =         call_low
+        self.call_mode =        call_mode
+        self.call_high =        call_high
+        self.long_call_low =    long_call_low
+        self.long_call_mode = long_call_mode
+        self.long_call_high = long_call_high    
+
+        #Assign peak hours of traffic in bank
         self.peak_min = peak_min
         self.peak_min_standard_deviation = peak_min_standard_deviation
         
@@ -182,6 +209,16 @@ class Experiment:
 
         # initialise sampling objects
         self.init_sampling()
+
+        if self.base_var_iat >= self.base_mean_iat:
+            raise ValueError(
+                f"`base_var_iat` ({self.base_var_iat}) must be smaller than `base_mean_iat` ({self.base_mean_iat})")
+        
+        if self.long_transact_base_var_iat >= self.long_transact_base_mean_iat:
+            raise ValueError(
+                f"`long_transact_base_var_iat` ({self.long_transact_base_var_iat }) must be smaller than `long_transact_base_mean_iat` ({self.long_transact_base_mean_iat })")
+
+        
 
     def set_random_no_set(self, random_number_set):
         """
@@ -208,7 +245,7 @@ class Experiment:
 
         # call inter-arrival times
         self.arrival_dist = Exponential(
-            self.mean_iat, random_seed=self.seeds[0]
+            self.base_mean_iat, random_seed=self.seeds[0]
         )
 
         # duration of call triage
@@ -235,7 +272,13 @@ class Experiment:
         """
         # variable used to store results of experiment
         self.results = {}
-        self.results["waiting_times"] = []
+        self.results['short_transact_time_of_arrival'] = []
+        self.results["short_transact_waiting_times"] = []
+
+        self.results['long_transact_time_of_arrival'] = []
+        self.results['long_transact_waiting_times'] = []
+
+        self.results['outside_waiting_times_time_of_arrival'] = []
         self.results['outside_waiting_times'] = []
 
         # total operator usage time for utilisation calculation.
@@ -267,6 +310,8 @@ def service(identifier, env, args, customer_type):
 
         outside_wait_time = env.now - start_wait
         args.results['outside_waiting_times'].append(outside_wait_time)
+        args.results['outside_waiting_times_time_of_arrival'].append(start_wait)
+        
 
         if customer_type=="short":
 
@@ -275,7 +320,9 @@ def service(identifier, env, args, customer_type):
 
                 wait_time = env.now - start_wait
 
-                args.results['waiting_times'].append(wait_time)
+                args.results['short_transact_waiting_times'].append(wait_time) 
+                #transaction waiting times are inclusive of the outside waiting time
+                args.results['short_transact_time_of_arrival'].append(start_wait)
 
                 call_duration = args.call_dist.sample()
 
@@ -283,14 +330,17 @@ def service(identifier, env, args, customer_type):
                 yield env.timeout(call_duration)
 
                 args.results['total_short_transact_duration'] += call_duration
-
+                
         elif customer_type =='long':
             with args.long_operators.request() as long_req:
                 yield long_req
                 
                 wait_time = env.now - start_wait
 
-                args.results['waiting_times'].append(wait_time)
+                args.results['long_transact_waiting_times'].append(wait_time)
+                #long transaction waiting times are inclusive of outside waiting times
+                args.results['long_transact_time_of_arrival'].append(start_wait)
+
 
                 call_duration = args.long_call_dist.sample()
 
@@ -299,20 +349,23 @@ def service(identifier, env, args, customer_type):
 
                 args.results['total_long_transact_duration'] += call_duration
 
+                trace(
+                    f'long transaction {identifier} ended {env.now:.2f};' \
+                    + f'wait time was {wait_time:.2f}'
+                )
         else:
             raise ValueError(f"Unknown customer type: {customer_type}")
         
-
 def bell_curve(x, mu, sigma):
 
     return np.exp(-(x-mu)**2/(2*(sigma**2)))
 
-def get_dynamic_mean_iat(current_time, short_iat, long_iat, peak_min, peak_min_standard_deviation):
+def get_dynamic_mean_iat(current_time_, short_iat_, long_iat_, peak_min_, peak_min_standard_deviation_):
     """
     Returns a dynamic mean inter-arrival time based on current simulation time.
     """
 
-    mean_iat = short_iat + (long_iat-short_iat)*(1-bell_curve(current_time, peak_min, peak_min_standard_deviation))
+    mean_iat = short_iat_ + (long_iat_-short_iat_)*(1-bell_curve(current_time_, peak_min_, peak_min_standard_deviation_))
     return mean_iat
 
 def arriv_gen(env, args):
@@ -343,7 +396,6 @@ def long_arriv_gen(env, args):
 
         env.process(service(caller_count, env, args, customer_type='long'))
 
-
 def single_run(experiment, rep=0, rc_period=RESULTS_COLLECTION_PERIOD):
     #rep: replication number
 
@@ -364,10 +416,19 @@ def single_run(experiment, rep=0, rc_period=RESULTS_COLLECTION_PERIOD):
     env.process(long_arriv_gen(env, experiment))
     env.run(until=rc_period)
 
-    run_results['01_mean_wait_time'] = np.mean(experiment.results['waiting_times'])
+    #results that summarize the run for the day
+    run_results['01_mean_wait_time'] = np.mean(experiment.results['short_transact_waiting_times'])
     run_results['02_teller_util'] = (experiment.results['total_short_transact_duration'] / (rc_period * experiment.n_operators)) *100.0
     run_results['03_mean_outside_wait_time'] = np.mean(experiment.results['outside_waiting_times'])
     run_results['04_long_teller_util'] = (experiment.results['total_long_transact_duration'] / (rc_period * experiment.n_long_operators)) *100.0
+    
+    #parameters for plotting time series data
+    run_results['05_short_transact_wait_times'] = experiment.results['short_transact_waiting_times']
+    run_results['06_short_transact_time_of_arrival'] = experiment.results['short_transact_time_of_arrival']
+    run_results['07_outside_wait_times'] = experiment.results['outside_waiting_times']
+    run_results['08_outside_wait_times_time_of_arrival'] = experiment.results['outside_waiting_times_time_of_arrival']
+    run_results['09_long_transact_wait_times'] = experiment.results['long_transact_waiting_times']
+    run_results['10_long_transact_wait_times_time_of_arrival'] = experiment.results['long_transact_time_of_arrival']
 
     return run_results
 
@@ -443,6 +504,7 @@ def single_run_n_days(exp, n_days, random_toggle=False):
     If random_toggle=True, a seed sequence is generated and used.
     Each seed in the seed_sequence is used in the replication value (rep) of the single_run command.'''
     #random_toggle= False by default
+    #To do: make it possible to get time series data from time stamps?(?)
     #if random_toggle is on, the run uses different seeds for each day
 
     ##seed sequence is fixed [0, 1, 2, ...] if random_toggle=False
@@ -465,4 +527,3 @@ def single_run_n_days(exp, n_days, random_toggle=False):
 
     return pd.DataFrame.from_dict(run_dict_transform)
     
-
