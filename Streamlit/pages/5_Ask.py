10eee536d6fb9042d834c49160bee70f07ebcb24
David Daffon <daviddaffon921@gmail.com>
2025-08-24 22:03:39 +0800

    cleaned up Ask

--- Streamlit/pages/5_Ask.py
+++ Streamlit/pages/5_Ask.py
@@ -1,4 +1,4 @@
-# Preliminaries
+### Preliminaries
 import os
 from dotenv import load_dotenv
 
@@ -21,19 +21,21 @@ from langchain_core.tools import tool
 # Import supabase.db
 from supabase.client import Client, create_client
 
+### Main
+
 # Load environment variables
-file_path = 'C:/David/000 Work Prep and Independent Proj/'
+file_path = 'C:/David/000 Work Prep and Independent Proj/' # Replace with path of DIBS on your device
 load_dotenv(dotenv_path=file_path + "/DIBS/gen_AI/.env")  
 
-# initiating supabase
+# Initiating supabase
 supabase_url = os.environ.get("SUPABASE_URL")
 supabase_key = os.environ.get("SUPABASE_SERVICE_KEY")
 supabase: Client = create_client(supabase_url, supabase_key)
 
-# initiating embeddings model
+# Initiating embeddings model
 embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 
-# initiating vector store
+# Initiating vector store
 vector_store = SupabaseVectorStore(
     embedding=embeddings,
     client=supabase,
@@ -41,15 +43,15 @@ vector_store = SupabaseVectorStore(
     query_name="match_documents",
 )
  
-# initiating llm
+# Initiating llm
 llm = ChatOpenAI(model="gpt-4o-mini", 
                  max_completion_tokens=None,
                  temperature=0)
 
-# pulling prompt from hub
+# Pulling prompt from hub
 prompt = hub.pull("hwchase17/openai-functions-agent")
 
-# creating the retriever tool
+# Creating the retriever tool
 @tool(response_format="content_and_artifact")
 def retrieve(query: str):
     """Retrieve information related to a query."""
@@ -59,24 +61,24 @@ def retrieve(query: str):
         for doc in retrieved_docs)
     return serialized, retrieved_docs
 
-# combining all tools
+# Combining all tools
 tools = [retrieve]
 
-# initiating the agent
+# Initiating the agent
 agent = create_tool_calling_agent(llm, tools, prompt)
 
-# create the agent executor
+# Create the agent executor
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-# initiating streamlit app
+# Initiating streamlit app
 st.set_page_config(page_title="Ask DIBS")
 st.title("Ask DIBS")
 
-# initialize chat history
+# Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# display chat messages from history on app rerun
+# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     if isinstance(message, HumanMessage):
         with st.chat_message("user"):
@@ -86,27 +88,20 @@ for message in st.session_state.messages:
             st.markdown(message.content)
 
 
-# create the bar where we can type messages
+# Create the bar where we can type messages
 user_question = st.chat_input("How may I help you?")
 
 
-# did the user submit a prompt?
+# Did the user submit a prompt?
 if user_question:
-
-    # add the message from the user (prompt) to the screen with streamlit
+    # Add the message from the user (prompt) to the screen with streamlit
     with st.chat_message("user"):
         st.markdown(user_question)
-
         st.session_state.messages.append(HumanMessage(user_question))
-
-
-    # invoking the agent
+    # Invoking the agent
     result = agent_executor.invoke({"input": user_question, "chat_history":st.session_state.messages})
-
     ai_message = result["output"]
-
-    # adding the response from the llm to the screen (and chat)
+    # Adding the response from the llm to the screen (and chat)
     with st.chat_message("assistant"):
         st.markdown(ai_message)
-
         st.session_state.messages.append(AIMessage(ai_message))
